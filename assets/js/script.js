4cfdd16c235e47c6110d9924d5bc3ac255790c19
viniciuspani <viniciuspani@hotmail.com>
2022-08-30 13:50:00 -0300

    commit inicial site

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+    //mascara telefone
+    $("#inputtel").mask("(99) 99999-9999");
+});
+
+function VerificaEmail(email) {
+    let [], emailVerificar = email;
+    array.forEach(emailVerificar => {
+        
+    });
+
+}
